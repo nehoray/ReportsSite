a3a70c37eb6d60eae3ccc054467929e1a187deb3
Maor Lahav <maorlahav5@cs.colman.ac.il>
2020-12-08 14:28:54 +0200

    menu

--- ReportsUI/src/app/app.module.ts
+++ ReportsUI/src/app/app.module.ts
@@ -1,27 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReportsViewComponent } from './views/reports-view/reports-view.component';
 import { NewReportViewComponent } from './views/new-report-view/new-report-view.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
-
+import { ToolBarComponent } from './toolbar/tool-bar/tool-bar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReportsViewComponent,
-    NewReportViewComponent
+    NewReportViewComponent,
+    ToolBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
