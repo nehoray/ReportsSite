937819a6ba15c41f18d52616b446cdb0f0b5d186
nehoray <nehoray99@gmail.com>
2020-12-08 22:56:33 +0200

    form

--- ReportsUI/src/app/app.module.ts
+++ ReportsUI/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {FormsModule,ReactiveFormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './material-module';
+import {NewReportViewComponent} from './views/new-report-view/new-report-view.component';
+import {ReportsViewComponent} from './views/reports-view/reports-view.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ReportsViewComponent } from './views/reports-view/reports-view.component';
-import { NewReportViewComponent } from './views/new-report-view/new-report-view.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material-module';
 
 
 @NgModule({
@@ -16,6 +18,9 @@ import { MaterialModule } from './material-module';
     NewReportViewComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    FlexLayoutModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
--- ReportsUI/src/app/views/new-report-view/new-report-view.component.ts
+++ ReportsUI/src/app/views/new-report-view/new-report-view.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component,OnInit} from '@angular/core';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
 @Component({
   selector: 'app-new-report-view',
   templateUrl: './new-report-view.component.html',
   styleUrls: ['./new-report-view.component.scss']
 })
 export class NewReportViewComponent implements OnInit {
+  reportForm: FormGroup = this.formBuilder.group({
+    title: [null, [Validators.required]],
+    location:[null,[Validators.required]],
+    unit:[null,[Validators.required]],
+    desc:[null,[Validators.required]],
+    participants:[null,[Validators.required]],
+    findings:[null,[Validators.required]],
+    conclusion:[null,[Validators.required]]
+  });
+  date = String(new Date())
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder
+    ) { }
 
-  ngOnInit(): void {
+  ngOnInit(
+  ): void {
+    console.log(this.date)
+  }
+  submit() {
+    console.log(this.reportForm.valid)
+    if (!this.reportForm.valid) {
+      return;
+    }
+    console.log(this.reportForm.value);
   }
 
 }
